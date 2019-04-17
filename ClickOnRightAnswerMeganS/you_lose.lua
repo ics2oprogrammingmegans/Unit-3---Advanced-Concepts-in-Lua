594b6ebe4a2af0e72bb7e49bafce4be33074e576
Megan Souannhaphanh <47784122+s256420@users.noreply.github.com>
2019-04-17 08:57:36 -0400

    Added new click on the right answer

--- ClickOnRightAnswerMeganS/you_lose.lua
+++ ClickOnRightAnswerMeganS/you_lose.lua
@@ -1,55 +1,45 @@
 -----------------------------------------------------------------------------------------
---
---splash_screen.lua
--- Created by: Your Name
+-- you_lose.lua
+-- Created by: Gil Robern
+-- Modified by: Your Name
 -- Date: Month Day, Year
--- Description: This is the splash screen of the game. It displays the 
--- company logo that...
+-- Description: This shows the player that they lost the game and plays a booing sound.
 -----------------------------------------------------------------------------------------
 
--- Use Composer Library
+-----------------------------------------------------------------------------------------
+-- INITIALIZATIONS
+-----------------------------------------------------------------------------------------
+
+-- Calling Composer Library
 local composer = require( "composer" )
 
--- Name the Scene
-sceneName = "splash_screen"
+local widget = require( "widget" )
 
 -----------------------------------------------------------------------------------------
 
--- Create Scene Object
+-- Naming Scene
+sceneName = "you_lose"
+
+-----------------------------------------------------------------------------------------
+
+-- Creating Scene Object
 local scene = composer.newScene( sceneName )
 
-----------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
 -- LOCAL VARIABLES
 -----------------------------------------------------------------------------------------
- 
--- The local variable for the Platelogo
 
-local Platelogo = display.newImage("Images/CompanyLogo.png", 50, 50 )
-local scrollSpeedPlatelogo = -9
+-- local variables for the scene
+local bkg
 
--- The local variables for the sound
-local CrashSound = audio.loadSound( "Sounds/CrashSound.mp3")
-local CrashSoundChannel
---------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
---------------------------------------------------------------------------------------------
-
--- The function that moves the Platelogo across the screen
-local function movePlatelogo()
-    Platelogo.x = Platelogo.x + scrollSpeedPlatelogo
-    -- change the transparency of the ship every time it moves so that it fades out
-    Platelogo.alpha = Platelogo.alpha + 0.01
-
-end
-
--- The function that will go to the main menu 
-local function gotoMainMenu()
-    composer.gotoScene( "main_menu" )
-end
+-----------------------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------------------
 -- GLOBAL SCENE FUNCTIONS
 -----------------------------------------------------------------------------------------
+
 
 -- The function called when the screen doesn't exist
 function scene:create( event )
@@ -57,37 +47,25 @@ function scene:create( event )
     -- Creating a group that associates objects with the scene
     local sceneGroup = self.view
 
-    -- set the background to be black
-    display.setDefault( "background", 1, 1, 1  ) 
---[[
-    -- Move the plate to the middle of the screen form the top
-    transition.moveTo( Platelogo, {x = display.contentWidth/2, y = display.contentHeight/2 , time = 1500})
+    -- Display background
+    bkg = display.newImage("Images/You Lose.png")
+    bkg.x = display.contentCenterX
+    bkg.y = display.contentCenterY
+    bkg.width = display.contentWidth
+    bkg.height = display.contentHeight
+    -----------------------------------------------------------------------------------------     
 
-    -- Set the scale of the logo
-    Platelogo:scale(0.25, 0.25)
+    -- Associating display objects with this scene 
+    sceneGroup:insert( bkg )
+end
 
-    -- Create the visiblility
-    Platelogo.alpha = 1
-
-    -- set the initial x and y position of the Platelogo
-    Platelogo.x = display.contentWidth/2
-    Platelogo.y = display.contentHeight*1/10
---]]
-    -- Have the logo fade out 
-    transition.fadeOut( Platelogo, { time = 4500 } )
-
-    -- Insert objects into the scene group in order to ONLY be associated with this scene
-    sceneGroup:insert( Platelogo )
-
-end 
-
---------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
 
 -- The function called when the scene is issued to appear on screen
 function scene:show( event )
 
     -- Creating a group that associates objects with the scene
-    local sceneGroup = self.views
+    local sceneGroup = self.view
 
     -----------------------------------------------------------------------------------------
 
@@ -95,24 +73,19 @@ function scene:show( event )
 
     -----------------------------------------------------------------------------------------
 
-    -- Called when the scene is still off screen (but is about to come on screen).
     if ( phase == "will" ) then
-       
+        -- Called when the scene is still off screen (but is about to come on screen).
+
     -----------------------------------------------------------------------------------------
 
     elseif ( phase == "did" ) then
-        -- start the splash screen music
-        CrashSoundChannel = audio.play( CrashSound )
 
-        -- Call the movePlatelogo function as soon as we enter the frame.
-        Runtime:addEventListener("enterFrame", movePlatelogo)
-
-        -- Go to the main menu screen after the given time.
-        timer.performWithDelay ( 3000, gotoMainMenu)          
-        
+        -- Called when the scene is now on screen.
+        -- Insert code here to make the scene come alive.
+        -- Example: start timers, begin animation, play audio, etc.
     end
 
-end --function scene:show( event )
+end
 
 -----------------------------------------------------------------------------------------
 
@@ -121,26 +94,25 @@ function scene:hide( event )
 
     -- Creating a group that associates objects with the scene
     local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
     local phase = event.phase
 
     -----------------------------------------------------------------------------------------
 
-    -- Called when the scene is on screen (but is about to go off screen).
-    -- Insert code here to "pause" the scene.
-    -- Example: stop timers, stop animation, stop audio, etc.
-    if ( phase == "will" ) then  
+    if ( phase == "will" ) then
+        -- Called when the scene is on screen (but is about to go off screen).
+        -- Insert code here to "pause" the scene.
+        -- Example: stop timers, stop animation, stop audio, etc.
 
     -----------------------------------------------------------------------------------------
 
-    -- Called immediately after scene goes off screen.
     elseif ( phase == "did" ) then
-        
-        -- stop the jungle sounds channel for this screen
-      audio.stop (CrashSoundsChannel) 
-      
+        -- Called immediately after scene goes off screen.
     end
 
-end --function scene:hide( event )
+end
 
 -----------------------------------------------------------------------------------------
 
@@ -156,7 +128,7 @@ function scene:destroy( event )
     -- Called prior to the removal of scene's view ("sceneGroup").
     -- Insert code here to clean up the scene.
     -- Example: remove display objects, save state, etc.
-end -- function scene:destroy( event )
+end
 
 -----------------------------------------------------------------------------------------
 -- EVENT LISTENERS
@@ -171,3 +143,4 @@ scene:addEventListener( "destroy", scene )
 -----------------------------------------------------------------------------------------
 
 return scene
+
